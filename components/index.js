aaf8e91c6a2fbfb7de5106966a5e7f00b8886454
jayakrishna-10 <96993255+jayakrishna-10@users.noreply.github.com>
2025-07-08 22:20:57 +0530

    Create index.js

---
+++ components/index.js
@@ -0,0 +1,7 @@
+// Export all components for easier imports
+export { default as CustomersTab } from './CustomersTab'
+export { default as LoansTab } from './LoansTab'
+export { default as RecoveriesTab } from './RecoveriesTab'
+export { default as ChilliesTab } from './ChilliesTab'
+export { default as LoadingSpinner } from './LoadingSpinner'
+export { default as ErrorBoundary } from './ErrorBoundary'
